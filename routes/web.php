8ab3a75daa5dd286470a55501a7cfda02415c798
Matthew Worrall <matthew.lloyd.worrall@gmail.com>
2026-03-18 16:35:15 +0000

    Front end

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ShopController::class, 'index']);
-Route::get('/shops/create', [ShopController::class, 'create']);
+Route::post('/', [ShopController::class, 'canDeliver'])->name('shop.can-deliver');
+Route::get('/shops/create', [ShopController::class, 'create'])->name('shop.create');
 Route::post('/shops/create', [ShopController::class, 'store']);
